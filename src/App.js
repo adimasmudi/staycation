b9f2d5a227e152155f5920ee22c711c8fac8f279
adimasmudi <adimasmudi@gmail.com>
2022-07-13 21:58:36 +0700

    finish checkout UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import 'assets/scss/style.scss';
+import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
 import Example from "pages/Example";
 import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
 
 function App() {
-  return <div className="App">
+  return (
+    <div className="App">
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
         <Route exact path="/properties/:id" component={DetailsPage}></Route>
-        <Route exact path="/example" component={Example}></Route>
+        <Route exact path="/checkout" component={Checkout}></Route>
       </Router>
     </div>
-  ;
+  );
 }
 
 export default App;
--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,35 +1,49 @@
-import React, { Component } from 'react'
-import Header from 'parts/Header'
-import PageDetailTitle from 'parts/PageDetailTitle'
-import Testimonials from 'parts/Testimonials';
-import Footer from 'parts/Footer';
+import React, { Component } from "react";
+import Header from "parts/Header";
+import PageDetailTitle from "parts/PageDetailTitle";
+import FeaturedImage from "parts/FeaturedImage";
+import PageDetailDescription from "parts/PageDetailDescription";
+import BookingForm from "parts/BookingForm";
+import Categories from "parts/Categories";
+import Testimonials from "parts/Testimonials";
+import Footer from "parts/Footer";
+import { Fade } from "react-reveal";
 
-import itemDetails from 'json/itemDetails.json';
+import itemDetails from "json/itemDetails.json";
 
 export default class DetailsPage extends Component {
-    componentDidMount(){
-        window.title = 'Details Page';
-        window.scrollTo(0,0);
-    }
-    
-    render() {
-        const breadcrumb = [
-            { pageTitle : "Home", pageHref : ""},
-            { pageTitle : "House Details", pageHref : ""}
-        ];
+  componentDidMount() {
+    window.title = "Details Page";
+    window.scrollTo(0, 0);
+  }
 
-        console.log(itemDetails)
-        return (
-            <>
-                <Header {...this.props}></Header>
-                <PageDetailTitle
-                    data={itemDetails}
-                    breadcrumb={breadcrumb}
-                    
-                >
+  render() {
+    const breadcrumb = [
+      { pageTitle: "Home", pageHref: "" },
+      { pageTitle: "House Details", pageHref: "" },
+    ];
 
-                </PageDetailTitle>
-            </>
-        )
-    }
+    return (
+      <>
+        <Header {...this.props} />
+        <PageDetailTitle data={itemDetails} breadcrumb={breadcrumb} />
+        <FeaturedImage data={itemDetails.imageUrls} />
+        <section className="container">
+          <div className="row">
+            <div className="col-7 pr-5">
+              <PageDetailDescription data={itemDetails} />
+            </div>
+            <div className="col-5">
+              <BookingForm itemDetails={itemDetails} />
+            </div>
+          </div>
+        </section>
+        <Fade bottom>
+          <Categories data={itemDetails.categories} />
+        </Fade>
+        <Testimonials data={itemDetails.testimonial} />
+        <Footer />
+      </>
+    );
+  }
 }
